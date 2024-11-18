f2085c88a1c01c7785a379b49381a683fc443ad6
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-18 18:25:28 +0700

    feat: update admin controller crud

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { clerkMiddleware } from '@clerk/express';
+import fileUpload from 'express-fileupload';
+import path from 'path';
 
 import { connectDB } from './lib/db.js';
 
@@ -13,12 +15,23 @@ import statRoutes from './routes/stat.route.js';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse req.body
 
 app.use(clerkMiddleware()); //this will add auth to req obj => req.auth.userId
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: path.join(__dirname, 'tmp'),
+        createParentPath: true,
+        limits: {
+            fileSize: 10 * 1024 * 1024 // 10MB  max file size
+        }
+    })
+);
 
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
@@ -26,6 +39,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statRoutes);
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message:
+            process.env.NODE_ENV === 'production'
+                ? 'Internal server error'
+                : err.message
+    });
+});
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
