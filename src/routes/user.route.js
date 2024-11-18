c548e7222c2c4882e6b2cca66d6156affe086019
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-18 19:35:56 +0700

    feat: update user routes and controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
+import { getAllUsers, getMessages } from '../controller/user.controller.js';
 
 const router = Router();
 
-router.get('/', protectRoute, (req, res) => {
-    req.auth.userId;
-    res.send('User route with GET method');
-});
+router.get('/', protectRoute, getAllUsers);
+router.get('/messages/:userId', protectRoute, getMessages);
 
 export default router;
